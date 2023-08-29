27267291a80e7ed9cfd8ef2be45e8d9f8bf84673
kirillbrujah <kirillbrujah@gmail.com>
2023-08-29 17:05:04 +0200

    Create Abstract Factory for orientation

--- lib/ui/widgets/orientation_factory.dart
+++ lib/ui/widgets/orientation_factory.dart
@@ -1,4 +1,11 @@
 import 'cards.dart';
 import 'package:flutter/material.dart';
 
+abstract class OrientationFactory {
+  DataCard buildDataCard({
+    required String title,
+    required String description,
+    required String imagePath,
+  });
+}
 
